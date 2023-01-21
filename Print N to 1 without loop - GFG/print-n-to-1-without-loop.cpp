6ce84de1e34ccde5acfb28bbcc048144fffc4ba6
Abhishek Mishra <112603898+Abhicodes55@users.noreply.github.com>
2023-01-22 00:11:11 +0530

    Added solution - LeetHub

---
+++ Print N to 1 without loop - GFG/print-n-to-1-without-loop.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+  int count;
+    void printNos(int N) {
+        // code here
+        
+        if(N<=0){
+            return;
+        }
+        cout<<N<<" ";
+        N--;
+        printNos(N);
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+/* Driver program to test printNos */
+int main() {
+    int T;
+
+    // taking testcases
+    cin >> T;
+
+    while (T--) {
+        int N;
+
+        // input N
+        cin >> N;
+        Solution ob;
+        // calling printNos() function
+        ob.printNos(N);
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
