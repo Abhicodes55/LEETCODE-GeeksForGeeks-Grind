23debe4068c749bcdebb9c3fe93ad98eb114b22a
Abhishek Mishra <112603898+Abhicodes55@users.noreply.github.com>
2023-02-08 23:24:53 +0530

    Added solution - LeetHub

---
+++ First and last occurrences of x - GFG/first-and-last-occurrences-of-x.cpp
@@ -0,0 +1,98 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+vector<int> find(int arr[], int n , int x )
+{      int a = -1;
+
+    int b = -1;
+
+    int low = 0,high = n-1;
+
+    while(low<=high){
+
+        int mid = low + (high-low)/2;
+
+        if(arr[mid]==x){
+
+            a = mid;
+
+            high = mid-1;
+
+        }
+
+        else if(arr[mid]>x){
+
+            high = mid-1;
+
+        }
+
+        else{
+
+            low = mid+1;
+
+        }
+
+    }
+
+    low = 0,high = n-1;
+
+    while(low<=high){
+
+        int mid = low + (high-low)/2;
+
+        if(arr[mid]==x){
+
+            b = mid;
+
+            low = mid+1;
+
+        }
+
+        else if(arr[mid]>x){
+
+            high = mid-1;
+
+        }
+
+        else{
+
+            low = mid+1;
+
+        }
+
+    }
+
+    vector<int>c;
+
+    c.push_back(a);
+
+    c.push_back(b);
+
+    return c;
+}
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,x;
+        cin>>n>>x;
+        int arr[n],i;
+        for(i=0;i<n;i++)
+        cin>>arr[i];
+        vector<int> ans;
+        ans=find(arr,n,x);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
