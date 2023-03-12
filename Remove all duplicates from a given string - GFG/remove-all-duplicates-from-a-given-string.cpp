3dbde22cb49e387fd508072950c86fa8fa0f9dd6
Abhishek Mishra <112603898+Abhicodes55@users.noreply.github.com>
2023-03-12 23:06:44 +0530

    Added solution - LeetHub

---
+++ Remove all duplicates from a given string - GFG/remove-all-duplicates-from-a-given-string.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	string removeDuplicates(string str) {
+	      unordered_map<char,int>m;
+
+     string k="";
+
+     for(int i=0;i<str.size();i++){
+
+         if(m[str[i]]==0){
+
+             k+=str[i];
+
+             m[str[i]]++;
+
+         }
+
+     }
+
+     return k;
+
+ }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string str;
+        cin >> str;
+        Solution ob;
+        auto ans = ob.removeDuplicates(str);
+
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
