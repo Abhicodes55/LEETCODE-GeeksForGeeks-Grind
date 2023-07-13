e0008d83c62574b33bf8acbc7552abdaab9ee548
Abhishek Mishra <112603898+Abhicodes55@users.noreply.github.com>
2023-07-13 22:54:18 +0530

    Added solution - LeetHub

---
+++ Unique Number of Occurrences - GFG/unique-number-of-occurrences.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+    public:
+    bool isFrequencyUnique(int n, int arr[])
+    {  if(n==1)return 1;
+        unordered_map<int,int>map;
+        for(int i=0;i<n;i++){
+            map[arr[i]]++;
+        }
+        set<int>set;
+        int count=0;
+        for(auto it:map){
+            count++;
+            set.insert(it.second);
+        }
+        return count==set.size();
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        Solution ob;
+        bool ans=ob.isFrequencyUnique(n,arr);
+        if(ans)
+            cout<<1<<endl;
+        else
+            cout<<0<<endl;
+    }
+}
+// } Driver Code Ends
